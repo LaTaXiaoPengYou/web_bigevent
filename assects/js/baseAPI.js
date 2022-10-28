7dcef37dd2ed1935a3045c9fea368516bc76903c
CBX <2606415026@qq.com>
2022-10-28 20:30:42 +0800

    更新

--- assects/js/baseAPI.js
+++ assects/js/baseAPI.js
@@ -6,7 +6,7 @@
 */
 $.ajaxPrefilter(function(options){
     // 在发起真正的 Ajax 请求之前 统一拼接请求的根路径
-    options.url = 'http://www.liulongbin.top:3007'+options.url
-    // options.url = 'http://127.0.0.1:2022'+options.url
+    // options.url = 'http://www.liulongbin.top:3007'+options.url
+    options.url = 'http://127.0.0.1:2022'+options.url
     console.log(options.url);
 })
